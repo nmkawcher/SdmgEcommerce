ecb6d85298626c38bb9645e539ba9f14b7945939
nmkawcher <nmkawcher112@gmail.com>
2021-12-09 10:31:14 +0600

    Initial commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/introduction.png",
+            height: 300,width: 300,),
+            SizedBox(height: 15,),
+            Text("Well Come to SDMG E-commerce",)
+
+          ],
+        ),
+      ),
+    );
+  }
+}
